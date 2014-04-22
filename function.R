4ab82a7722c0b11f9239d8cbaae0fb0beaf3cdd6
Christian I. Ponce <christian.ponce@foodgeek.pro>
2014-04-21 21:53:41 -0500

    Primer boceto. Entregado a las 9:55 pm del día 21 de abril de 2014. Creacion del archivo .Rnw

--- function.R
+++ function.R
@@ -1,5 +1,7 @@
 # ---- X* ----
 randx=function(n, j, mu, sigma){
+  #n=número de filas
+  #j=número de columnas
   X=matrix(ncol=j, nrow=n)
   uno=rep(1,n)
     for(i in 1:n){
@@ -7,7 +9,6 @@ randx=function(n, j, mu, sigma){
       X[i,j]=rnorm(1, mean=(mu-j), sd=sigma)
     }#cierra for(j in 1:j){
   }#cierra for(i in 1:n){
-##Aplicar la función para generar X y Y  
   return(X=cbind(uno,X))
 }
 
@@ -22,21 +23,21 @@ mu=X%*%beta
 
 # ---- Yi ----
 yi=rlnorm(25, meanlog=mu, sdlog=1)
-c=sort(yi)[20]
 
 # ---- censura ----
 c=sort(yi)[20]
-cen=yi>c
-scen=yi<=c
+cen=yi>c # qué datos son sensurados
+scen=yi<=c # qué datos no son cesurados
 yi[cen]=exp(25)
 
 # ---- particion ----
-Xc=X[cen,]
-Xsc=X[scen,]
+Xc=X[cen,] ##No se usa
+Xsc=X[scen,] ##No se usa
 
+# ---- trans ---- 
 y=log(yi)
-c=25
-# ---- imput----
+
+# ---- em.censurada ----
 em.censurada=function(y,x,c,n){
   #y vector de datos censurados ~\sim N(mu_i,sigma)
   #x matriz de datos [1,x*]
@@ -45,25 +46,40 @@ em.censurada=function(y,x,c,n){
   no.cen=y<c
   ly=lm(y[no.cen]~X[no.cen,-1])
   b=coef(ly)
-  sigma=deviance(ly)/df.residual(ly)
+  sigma=sqrt(deviance(ly)/(sum(no.cen)))
   for(i in 1:n){
     media=c(X%*%b)
     z=(y-media)/sigma
     sesgo=sigma*dnorm(z)/(1-pnorm(z))
-    #y.old=y.new
     y[cen]=c(media+sesgo)[cen]
     ly=lm(y~X[,-1])
     b=coef(ly)
     suma=sum((z*sesgo*sigma+sigma^2-sesgo^2)[cen])
-    sigma=(deviance(ly)+suma)/df.residual(ly)
-    cat(media[cen],fill=T)
-    #cat(sesgo, fill=T)
+    sigma=sqrt((deviance(ly)+suma)/nrow(X))
   if(i==n){
-    return(list(coefficients=b, variance=sigma,y=y))
+    return(ly)
   }#cierra if(i==n){
   } #for(i in 1:n){
-}#cierra function(y,xc,xsc)
+}#cierra function(y,x,c,n)
 
-y
-# ---- imput ----
-em.censurada(y,X, 25, 9)
+# ---- input ----
+m=2
+yc=em.censurada(y, X, 25, n=m) #n=1 el sesgo es muy peqeño
+
+# ---- coeficientes
+beta=matrix(yc$coefficients,nrow=1)
+colnames(beta)=c("b0","b1","b2","b3","b4","b5","b6",
+                                "b7","b8","b9","b10")
+xtable(beta,caption="Coeficientes estimados tras $m$ iteraciones",
+       label="tab:coef",digits=4)
+
+# ---- ycen ----
+yc$fit[cen]
+
+# ---- input2 ----
+m=10
+yc=em.censurada(y, X, 25, n=m) #n=1 el sesgo es muy peqeño
+
+# ---- ycen2 ----
+xtable(matrix(yc$fit[cen],ncol=5),
+       caption="Estimación de las observaciones $y_i$ truncadas")
